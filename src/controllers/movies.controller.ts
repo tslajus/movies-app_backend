36b81e4fab5f0ef3379980fb5d024045e926bb13
Tadas Šlajus <tlajus@gmail.com>
2023-04-13 14:23:41 +0300

    ability to retrieve list of movies from TMDB database;

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,10 +1,23 @@
 import express from 'express';
-import movieList from '../../dist/src/data/movieList.json';
+import axios from 'axios';
+import { movieConverter } from '../converters/movie.converter';
+
 import movie from '../../dist/src/data/movie.json';
 
-const getMovies = (_req: express.Request, res: express.Response): void => {
-  const movies: Movies = movieList;
-  res.json(movies);
+const getMovies = async (_req: express.Request, res: express.Response): Promise<void> => {
+  try {
+    const response = await axios.get(`${process.env.BASE_URL}${process.env.API_KEY}`);
+    const tmdbMovies = response.data.results;
+    const movies: Movies = {
+      page: 1,
+      totalPages: response.data.total_pages,
+      movies: tmdbMovies.map(movieConverter),
+    };
+    res.json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 const getMovieDetails = (_req: express.Request, res: express.Response): void => {
