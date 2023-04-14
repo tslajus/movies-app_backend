234574da6944097eb57efbea1e052de20da00ecc
Tadas Šlajus <tlajus@gmail.com>
2023-04-14 06:55:53 +0300

    Cache for Movies;

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -4,8 +4,15 @@ import { movieConverter } from '../converters/movie.converter';
 
 import movie from '../../dist/src/data/movie.json';
 
-const getMovies = async (_req: express.Request, res: express.Response): Promise<void> => {
+let cachedMovies: Movies;
+
+const getMovies = async (_req: express.Request, res: express.Response): Promise<express.Response> => {
   try {
+    if (cachedMovies) {
+      console.log('Returning movies from cache');
+      return res.json(cachedMovies);
+    }
+
     const response = await axios.get(`${process.env.BASE_URL}${process.env.API_KEY}`);
     const tmdbMovies = response.data.results;
     const movies: Movies = {
@@ -13,10 +20,14 @@ const getMovies = async (_req: express.Request, res: express.Response): Promise<
       totalPages: response.data.total_pages,
       movies: tmdbMovies.map(movieConverter),
     };
-    res.json(movies);
+
+    cachedMovies = movies;
+    console.log('Returning movies from API');
+
+    return res.json(movies);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
