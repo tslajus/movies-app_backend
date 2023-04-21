31fd004814516279aa977f70c14e51e5eb8634d6
Tadas Šlajus <tslajus@gmail.com~>
2023-04-21 06:26:05 +0300

    Health Test Fix;

--- test/integration/health.test.ts
+++ test/integration/health.test.ts
@@ -1,26 +1,30 @@
 import httpStatus from 'http-status';
 import supertest from 'supertest';
+import mongoose from 'mongoose';
 
 import app from '../../src/app';
 
 const expectedResponse = {
   health: true,
-  version: 'v0.1',
+  version: 'v0.2',
+  mongo: true,
 };
 
 describe('Health API', () => {
   const request = supertest(app);
 
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
   describe('GET /health', () => {
-    it('should return health status', (done) => {
-      request
-        .get('/health')
-        .expect(httpStatus.OK)
-        .then((res) => {
-          expect(res.body).toEqual(expectedResponse);
-          done();
-        })
-        .catch(done);
+    it('should return health status', async () => {
+      const res = await request.get('/health').expect(httpStatus.OK);
+      expect(res.body).toEqual(expectedResponse);
     });
   });
 });
